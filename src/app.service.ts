4cea4d957d5d7b5bdfe5fff391a5c55ce8ed5e6a
Osunde Goodluck Michael <osundemichael7@gmail.com>
2026-09-07 18:23:33 +0100

    reploying the server

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      author: 'MichaelKing',
+      message:
+        'Hello World from github-action, ecr and ecs with applicaton load balancer all configured and working fine',
+    };
   }
 }
